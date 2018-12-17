8289792e90aefca9d8c43e415b7507c26c2be726
Anninster <anninster.khttk@gmail.com>
2018-12-17 17:33:27 +0500

    Add files via upload

---
+++ semester2/home 1/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "cout << "
+                double a;
+                        double b;
+                        double c;
+                        int d;
+                        cout << "enter num1 = ";
+                        cin >> a;
+                        cout << "\nenter (num2 =0)num2 = ";
+                        cin >> b;
+                        cout << "\nwhat do you want to do" << "\n+=1 -=2 *=3 /=4\n";
+                        cin >> d;
+                        if (d==1)
+                            {
+                                c=a+b;
+                            }
+                        if (d==2)
+                            {
+                                c=a-b;
+                            }
+                        if (d==3)
+                            {
+                                c=a*b;
+                            }
+                        if (d==4)
+                            {
+                                c=a/b;
+                            }
+                        cout << "result = " << c;
+
+
+
+                ";
+                 ";
+    return 0;
+}
---
+++ semester2/input output/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string hello = "";
+    string world = "";
+
+    cout << "what you want to display; ";
+    cin >> hello >> world;
+    cout << hello << " " << world;
+    return 0;
+}
